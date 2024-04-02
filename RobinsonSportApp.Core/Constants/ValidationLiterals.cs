ce5240fb850dfa8184da09ad3dc6a27e8b465027
Arm_G <armen_g@purlin.com>
2024-04-03 02:19:31 +0400

    added contact page

---
+++ RobinsonSportApp.Core/Constants/ValidationLiterals.cs
@@ -0,0 +1,9 @@
+﻿namespace RobinsonSportApp.Core.Constants;
+
+public static class ValidationLiterals
+{
+    public const int FirstNameMaxLength = 20;
+    public const int LastNameMaxLength = 20;
+    public const int QuestionMinLength = 10;
+    public const int QuestionMaxLength = 500;
+}
---
+++ RobinsonSportApp.Core/Models/ContactFormModel.cs
@@ -0,0 +1,19 @@
+﻿using RobinsonSportApp.Core.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace RobinsonSportApp.Core.Models;
+
+public class ContactFormModel
+{
+    [Required, MaxLength(ValidationLiterals.FirstNameMaxLength)]
+    public string FirstName { get; set; }
+
+    [Required, MaxLength(ValidationLiterals.LastNameMaxLength)]
+    public string LastName { get; set; }
+
+    [Required, RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")]
+    public string Email { get; set; }
+
+    [Required, MinLength(ValidationLiterals.QuestionMinLength), MaxLength(ValidationLiterals.QuestionMaxLength)]
+    public string Question { get; set; }
+}
