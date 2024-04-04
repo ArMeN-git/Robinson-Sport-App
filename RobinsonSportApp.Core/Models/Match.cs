7558a42e655652f10b63131aa936afb9f1824dc9
Work <Work@LAPTOP-DRK8O5DM>
2024-04-04 16:06:04 +0400

    Upcoming Matches

---
+++ RobinsonSportApp.Core/Models/Match.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobinsonSportApp.Core.Models
+{
+    public class Match
+    {
+        public string matchType { get; set; }
+        public DateTime date { get; set; }
+        public string place { get; set; }
+        public string location {  get; set; }
+        public string firstTeam { get; set; }
+        public string secondTeam {  get; set; }
+
+    }
+}
