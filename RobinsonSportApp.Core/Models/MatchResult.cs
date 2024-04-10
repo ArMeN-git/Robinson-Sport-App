ffd4d076dd134f8e36d1b50e583592edcef9422a
Arm_G <armen_g@purlin.com>
2024-04-10 18:52:54 +0400

    contact page margins fix, initial results page

---
+++ RobinsonSportApp.Core/Models/MatchResult.cs
@@ -0,0 +1,15 @@
+﻿using RobinsonSportApp.Core.Enums;
+
+namespace RobinsonSportApp.Core.Models;
+
+public class MatchResult
+{
+    public int GameId { get; set; }
+    public string HomeTeamLogo { get; set; }
+    public string AwayTeamLogo { get; set; }
+    public string HomeTeamName { get; set; }
+    public string AwayTeamName { get; set; }
+    public int HomeTeamScore { get; set; }
+    public int AwayTeamScore { get; set; }
+    public SportCategory SportCategory { get; set; }
+}
