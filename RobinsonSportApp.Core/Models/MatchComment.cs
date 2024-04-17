9446659068757b63e19e6d94fee1684671a2238e
Arm_G <armen_g@purlin.com>
2024-04-17 18:34:30 +0400

    create page to show single match information, addtional info, comments section, responsive design

---
+++ RobinsonSportApp.Core/Models/MatchComment.cs
@@ -0,0 +1,8 @@
+﻿namespace RobinsonSportApp.Core.Models;
+
+public class MatchComment
+{
+    public string AuthorName { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
