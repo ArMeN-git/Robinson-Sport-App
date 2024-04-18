f4def9a2bc3b8db9929fb79539a7211aa406a30c
Work <Work@LAPTOP-DRK8O5DM>
2024-04-18 15:38:08 +0400

    Added filter-bar feature

--- RobinsonSportApp.Core/Models/Association.cs
+++ RobinsonSportApp.Core/Models/Association.cs
@@ -16,7 +16,7 @@ namespace RobinsonSportApp.Core.Models
 
 		public string address { get; set; }
 
-		public int phoneNumber {  get; set; }
+		public string phoneNumber {  get; set; }
 
 		public string email {  get; set; }
 
