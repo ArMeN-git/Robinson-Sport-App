7bcb40ba1fbfb62ee269c8fed6f0e9bd7e0d636c
Arm_G <armen_g@purlin.com>
2024-04-22 14:49:49 +0400

    create DAL project, EF Core 8 integration, connect to local database

---
+++ RobinsonSportApp.Data/Configurations/ConnectionStringsConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace RobinsonSportApp.Data.Configurations;
+
+public class ConnectionStringsConfig
+{
+    public const string ConnectionStrings = nameof(ConnectionStrings);
+    public string RobinsonSportAppDatabase { get; set; }
+}
+
---
+++ RobinsonSportApp.Data/RobinsonSportAppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RobinsonSportApp.Data;
+
+public class RobinsonSportAppDbContext(DbContextOptions<RobinsonSportAppDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
