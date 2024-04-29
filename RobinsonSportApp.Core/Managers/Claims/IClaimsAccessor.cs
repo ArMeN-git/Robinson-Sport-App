5a5b03832e252f6ccb270640ff086de217dacb45
Arm_G <armen_g@purlin.com>
2024-04-29 20:26:54 +0400

    claimsaccessor setup to get user claims when necessarym authorize setup for comments part

---
+++ RobinsonSportApp.Core/Managers/Claims/IClaimsAccessor.cs
@@ -0,0 +1,7 @@
+﻿namespace RobinsonSportApp.Core.Managers.Claims;
+
+public interface IClaimsAccessor
+{
+    int UserId { get; set; }
+    string Email { get; set; }
+}
--- RobinsonSportApp.Core/Managers/EventComments/Models/AddCommentModel.cs
+++ RobinsonSportApp.Core/Managers/EventComments/Models/AddCommentModel.cs
@@ -1,8 +1,12 @@
-﻿namespace RobinsonSportApp.Core.Managers.EventComments.NewFolder;
+﻿using RobinsonSportApp.Core.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace RobinsonSportApp.Core.Managers.EventComments.NewFolder;
 
 public class AddCommentModel
 {
     public int UserId { get; set; }
     public long EventId { get; set; }
+    [Required, MinLength(ValidationLiterals.CommentMinLength), MaxLength(ValidationLiterals.CommentMaxLength)]
     public string Comment { get; set; }
 }
